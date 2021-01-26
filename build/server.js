d82f02fe5d93cd3f0e6d2a9842756eaa4afb62e1
Ignacio <ignacionrmesa19>
2021-01-26 17:22:20 +0100

    First Commit

--- build/server.js
+++ build/server.js
@@ -27,6 +27,13 @@ class Server {
             this.app.use(express_1.default.json()); // para que nuestro servidor entienda
             // los formatos json desde clientes
             this.app.use(morgan_1.default('dev')); // Para que muestre las url invocadas
+            this.app.use(function (req, res, next) {
+                res.header("Access-Control-Allow-Origin", '*');
+                res.header("Access-Control-Allow-Credentials", 'true');
+                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+                next();
+            });
         });
     }
     routes() {
--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,15 @@ class Server {
         this.app.use(express.json()) // para que nuestro servidor entienda
         // los formatos json desde clientes
         this.app.use(morgan('dev'))  // Para que muestre las url invocadas
+
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", '*');
+            res.header("Access-Control-Allow-Credentials", 'true');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+            next();
+        });
+
     }
 
     private routes(){
